fa36f3a45c548fba1782901be9c066df8d05b5c4
Yasar <yaltamiranoa@outlook.com>
2021-01-20 13:45:29 -0600

    Function 2 Finalizada

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
  * propiedades, en este último caso los valores de las propiedades deben 
  * ser comparados con una llamada recursiva de deepEqual.
  */
-function deepEqual(a, b) {
+/*function deepEqual(a, b) {
     if (typeof a === typeof b) {
         switch (typeof a) {
             case "string":
@@ -50,4 +50,39 @@ console.log('Test 3:', deepEqual(john, john)) // true
 console.log('Test 4:', deepEqual(john, { firstName: 'John', lastName: 'Doe' })) // true
 console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false
 console.log('Test 6:', deepEqual(john, { firstName: 'John', other: 'Mor' })) // false
-console.log('Test 7:', deepEqual(john, { firstName: 'John', lastName: 'Do' })) // false
+console.log('Test 7:', deepEqual(john, { firstName: 'John', lastName: 'Do' })) // false*/
+
+/**
+ * Function 2
+ * Escribir una función chunk que recibe un arreglo y un número 
+ * entero size. La función debe dividir el arreglo en múltiples 
+ * arreglos del tamaño determinado por size.
+ *  
+ */
+function chunk(array, size) {
+    var result = [];
+    var elements = [];
+    var position = 0;
+    if (size > 0) {
+        while (position < array.length) {
+            for (let i = 0;
+                (i < size) && (position < array.length); i++) {
+                elements.push(array[position]);
+                position++;
+            }
+            result.push(elements);
+            elements = [];
+        }
+    }
+
+    return result;
+};
+
+var data = [1, 2, 3, 4, 5, 6, 7, 8]
+
+console.log('Test 1:', chunk(data, 1)) // [[1], [2], [3], [4], [5], [6], [7], [8]]
+console.log('Test 2:', chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7, 8]]
+console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
+console.log('Test 4:', chunk(data, 4))
+console.log('Test 5:', chunk(data, 5))
+console.log('Test 6:', chunk(data, 9))
